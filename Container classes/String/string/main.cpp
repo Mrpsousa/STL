f90413348c9e12dd4ef392d5ea81bf3170878d54
Marcos Rogerio Pinheiro Sousa <mrpsousauesc@gmail.com>
2018-10-19 22:52:37 -0300

    string TODO

---
+++ Container classes/String/string/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+    string nome1, nome2;
+    string teste1 = "Maria";
+    string teste2 = "Joao";
+
+    nome1.append(teste1);
+    nome2.append("TOINHO");
+    nome1+= teste2;
+    cout << nome1.size() << endl;
+    cout << nome1 << endl;
+    cout << nome2 << endl;
+
+    //TODO
+    // Iterator
+    //begin
+    //end
+   
+    
+    //TODO
+    //Capacity
+    //size
+    //length
+    //clear
+    //empty
+    //shrink_to_fit
+
+    //TODO
+    //Element access
+    //at
+    //back
+    //front
+    
+    //TODO
+    //Modifiers
+    //operador +=
+    //append
+    //push_back
+    //assing
+    //insert
+    //erase
+    //replace
+    //swap
+    //pop_back
+
+
+    //TODO
+    //string operations
+    //c_str
+    //copy
+    //find
+    //rfind
+    //find_first_of
+    //find_last_of
+    //find_first_not_of
+    //find_last_not_of
+    //substr
+    //compare
+
+
+
+    return 0;
+}
